8573d2cea1b47783db787d9b621002a88bc8b3c8
Naveen Chauhan <naveen.chauhan010@gmail.com>
2016-07-21 10:13:48 +0530

    Initial Commit

---
+++ EmployeeRecord/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  EmployeeRecord
+//
+//  Created by Naveen Chauhan on 21/07/16.
+//  Copyright © 2016 com.naveen. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: AnyObject? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
